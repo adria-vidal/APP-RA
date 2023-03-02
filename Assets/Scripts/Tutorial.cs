e166b828995096bae7824b95577bf3bfdabcc45e
Adria Vidal <adriavidal1998@gmail.com>
2023-03-02 20:42:18 +0100

    funciones de activación y desactivación de imágenes y botones en la escena Tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -8,46 +8,66 @@ using TMPro;
 public class Tutorial : MonoBehaviour
 {
     
-     // Referencia al objeto de texto
+    // Referencia al objeto de texto
     public TextMeshProUGUI tutorialText;
-    // Referencia a la imagen
+    
+    // Referencia a las imágenes
     public Image infoImage;
     public Image soundImage;
     public Image recargaImage;
 
-
     // Referencias a los objetos de botón
     public Button buttonTutorial;
+    public Button buttonSiguiente;
     public Button buttonSaltar;
+
+    private int currentImage = 0;
 
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-
-    // Función que se ejecuta al pulsar el botón
+    // Función que se ejecuta al pulsar el botón "Tutorial"
     public void OnTutorialButtonPressed()
     {
-        // Desactivar el componente de TextMeshPro
         tutorialText.enabled = false;
-        // Desactivar los botones
-        buttonTutorial.gameObject.SetActive(false);
         buttonSaltar.gameObject.SetActive(false);
-
-        // Activar la imagen
+        buttonTutorial.gameObject.SetActive(false);
+        buttonSiguiente.gameObject.SetActive(true);
         infoImage.gameObject.SetActive(true);
     }
-    // Función que se ejecuta al hacer clic en la pantalla
-   // Función que se ejecuta al hacer clic en el panel
-    public void OnPanelClick()
-    {
-        // Cambiar la opacidad de la imagen actual
-        Color currentColor = GetComponentInChildren<Image>().color;
-        currentColor.a = 0.1f;
-        GetComponentInChildren<Image>().color = currentColor;
 
-        // Activar la segunda imagen
-        soundImage.gameObject.SetActive(true);
+    // Función que se ejecuta al pulsar el botón "Siguiente"
+    public void OnSiguienteButtonPressed()
+    {
+        // Desactivar la imagen actual
+        if (currentImage == 0)
+        {
+            infoImage.gameObject.SetActive(false);
+            // Cambiar la opacidad de la imagen siguiente
+            Color currentColor = soundImage.color;
+            currentColor.a = 0.8f;
+            soundImage.color = currentColor;
+            soundImage.gameObject.SetActive(true);
+        }
+        else if (currentImage == 1)
+        {
+            soundImage.gameObject.SetActive(false);
+            // Cambiar la opacidad de la imagen siguiente
+            Color currentColor = recargaImage.color;
+            currentColor.a = 0.8f;
+            recargaImage.color = currentColor;
+            recargaImage.gameObject.SetActive(true);
+            buttonSiguiente.GetComponentInChildren<TextMeshProUGUI>().text = "Continuar";
+        }
+        else
+        {
+            LoadNextScene();
+        }
+
+        // Incrementar el índice de imagen actual
+        currentImage++;
     }
+    
 }
